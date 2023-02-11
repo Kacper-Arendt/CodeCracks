e1709ec93d8037e91d1b98639eb00c004e2c0ce2
ArendtKacper <k.arendt@gridaly.com>
2023-02-11 13:48:53 +0100

    first commit

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+// @ts-ignore
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
