5537987bed4264c82d20257f0d6201b6092b66b3
ArendtKacper <k.arendt@gridaly.com>
2023-02-15 16:41:53 +0100

    [WIP] server - deploying

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,9 @@
 import { createServer } from 'src/server';
-import { log } from 'logger';
+import * as console from 'console';
 
 const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
-	log(`api running on ${port}`);
+	console.log(`api running on ${port}`);
 });
