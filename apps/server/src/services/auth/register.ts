8e07aea74f8c835529c1b23871c415ef0883e357
ArendtKacper <k.arendt@gridaly.com>
2023-02-24 08:56:24 +0100

    feat add prisma

--- apps/server/src/services/auth/register.ts
+++ apps/server/src/services/auth/register.ts
@@ -1,24 +1,33 @@
 import { Request, Response } from 'express';
 import { hash } from 'server-utils';
 
+import { PrismaClient } from 'prisma/client';
+const prisma = new PrismaClient();
+
 export const register = async (req: Request, res: Response) => {
 	try {
 		const { password, email } = req.body;
 
 		if (!password || !email) return res.status(404).json({ error: 'Data not provided' });
 
-		// const existingUser = await User.findOne({
-		// 	where: { email: email },
-		// });
+		const existingUser = await prisma.user.findFirst({
+			where: { email },
+		});
 
-		// if (existingUser) return res.status(404).json({ error: 'Email already exists' });
+		if (existingUser) return res.status(404).json({ error: 'Email already exists' });
 
 		const passwordHash = await hash(password);
 
-		// const newUser: User = await User.create({ passwordHash, email });
+		const result = await prisma.user.create({
+			data: {
+				hash: passwordHash,
+				email,
+			},
+		});
 
-		// return res.status(200).json({ id: newUser.id, email });
+		res.json({ id: result.id });
 	} catch (e) {
+		console.log(e);
 		res.status(500).json({ error: 'The server cannot create the user' });
 	}
 };
