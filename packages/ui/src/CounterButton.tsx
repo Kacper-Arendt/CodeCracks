20bc4bb6e8354cc8fa9d92c222067b379cb1a238
ArendtKacper <k.arendt@gridaly.com>
2023-02-24 22:37:02 +0100

    packages: add web-utils

--- packages/ui/src/CounterButton.tsx
+++ packages/ui/src/CounterButton.tsx
@@ -1,45 +1,35 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledCounterButton = styled.div`
+	display: flex;
+	flex-direction: column;
+	row-gap: 2rem;
+	padding: 2rem;
+
+	align-items: center;
+	justify-content: center;
+
+	button {
+		padding: 1rem;
+		background-color: transparent;
+		border: 2px solid ${({ theme }) => theme.primary};
+		border-radius: ${({ theme }) => theme.radius[2]};
+	}
+
+	p {
+		color: ${({ theme }) => theme.primary};
+	}
+`;
 
 export const CounterButton = () => {
 	const [count, setCount] = useState(0);
 	return (
-		<div
-			style={{
-				background: `rgba(0,0,0,0.05)`,
-				borderRadius: `8px`,
-				padding: '1.5rem',
-				fontWeight: 500,
-			}}
-		>
-			<p style={{ margin: '0 0 1.5rem 0' }}>
-				This component is from{' '}
-				<code
-					style={{
-						padding: '0.2rem 0.3rem',
-						background: `rgba(0,0,0,0.1)`,
-						borderRadius: '0.25rem',
-					}}
-				>
-					ui
-				</code>
-			</p>
-			<div>
-				<button
-					style={{
-						background: 'black',
-						color: 'white',
-						border: 'none',
-						padding: '0.5rem 1rem',
-						borderRadius: '0.25rem',
-						display: 'inline-block',
-						cursor: 'pointer',
-					}}
-					type="button"
-					onClick={() => setCount((c) => c + 1)}
-				>
-					Count: {count}
-				</button>
-			</div>
-		</div>
+		<StyledCounterButton>
+			<button type="button" onClick={() => setCount((c) => c + 1)}>
+				Click
+			</button>
+			<p>Count: {count}</p>
+		</StyledCounterButton>
 	);
 };
