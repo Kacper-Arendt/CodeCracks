3a20c511db60be23216f2787a0779e29f3abcd0a
ArendtKacper <k.arendt@gridaly.com>
2023-03-06 09:10:52 +0100

    apps.web: add register form test

--- apps/web/src/features/auth/components/__tests__/RegisterForm.test.tsx
+++ apps/web/src/features/auth/components/__tests__/RegisterForm.test.tsx
@@ -2,8 +2,38 @@ import { RegisterForm } from 'src/features/auth/components/RegisterForm';
 import { customRender, screen } from 'src/tests/testUtils';
 
 describe('Register form', () => {
-	it('renders form', () => {
+	beforeEach(() => {
 		customRender(<RegisterForm />);
+	});
+
+	it('renders form', () => {
+		expect(screen.getByRole('form')).toBeInTheDocument();
+		screen.debug();
+	});
+
+	it('renders email field', () => {
+		const emailInput = screen.getByRole('textbox', { name: /email/i });
+
+		expect(emailInput).toBeInTheDocument();
+		expect(emailInput).toHaveAttribute('type', 'text');
+
+		screen.debug();
+	});
+
+	it('renders password field', () => {
+		const passwordInput = screen.getByPlaceholderText('Enter your password');
+
+		expect(passwordInput).toBeInTheDocument();
+		expect(passwordInput).toHaveAttribute('type', 'password');
+
+		screen.debug();
+	});
+
+	it('renders submit button', () => {
+		const submitButton = screen.getByRole('button', { name: /submit/i });
+
+		expect(submitButton).toBeInTheDocument();
+		expect(submitButton).toHaveAttribute('type', 'submit');
 
 		screen.debug();
 	});
