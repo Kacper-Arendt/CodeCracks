f2a63e6e0dfff025dde3393d7a24ee822804ac57
ArendtKacper <k.arendt@gridaly.com>
2023-03-11 21:35:50 +0100

    apps.server: update build script

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -12,14 +12,7 @@ import { routes } from 'src/routes';
 export const createServer = () => {
 	const app = express();
 
-	app.all('*', function (req, res, next) {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-		next();
-	});
-
 	app.use(cors());
-	app.options('*', cors());
 	app.disable('x-powered-by');
 	app.use(morgan('dev'));
 	app.use(helmet());
