9746d9c60cb61551eaf745aea8010aef8f9ddb6d
ArendtKacper <k.arendt@gridaly.com>
2023-03-12 20:42:36 +0100

    apps.server: update build script

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -8,7 +8,6 @@ import { errorHandler } from 'src/utils/errorHandler';
 
 // ROUTES
 import { routes } from 'src/routes';
-import { register } from 'src/services/auth';
 
 export const createServer = () => {
 	const app = express();
@@ -20,18 +19,6 @@ export const createServer = () => {
 	app.use(express.urlencoded());
 	app.use(express.json());
 
-	app.all('*', function (req, res, next) {
-		res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
-		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-		res.header('Access-Control-Allow-Headers', 'Content-Type');
-		if ('OPTIONS' == req.method) {
-			res.sendStatus(200);
-		} else {
-			next();
-		}
-	});
-
-	app.post('/register', register);
 	app.use('/', routes);
 
 	app.get('*', (req, res) => {
