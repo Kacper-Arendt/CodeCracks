70e4c020999b0490c06bc94d5acd349f06936c0b
ArendtKacper <k.arendt@gridaly.com>
2023-03-12 20:44:44 +0100

    apps.web: uninstall axios

--- apps/web/src/features/auth/hooks/useRegister.ts
+++ apps/web/src/features/auth/hooks/useRegister.ts
@@ -1,8 +1,5 @@
-import axios from 'axios';
-
 // HOOKS
 import { useFetch } from 'web-utils';
-import { useEffect } from 'react';
 
 export const useRegister = () => {
 	const { fetchFn, loading } = useFetch();
@@ -14,24 +11,8 @@ export const useRegister = () => {
 		};
 		const onFinish = () => alert('account created successfully');
 
-		axios
-			.post('https://plankton-app-92stg.ondigitalocean.app/register', body)
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((err) => console.log(err));
-		// fetchFn({ url: 'auth/register', options: { method: 'POST', body }, onFinish, onError });
+		fetchFn({ url: 'auth/register', options: { method: 'POST', body }, onFinish, onError });
 	};
-
-	useEffect(() => {
-		axios
-			.post('https://plankton-app-92stg.ondigitalocean.app/register', { email: 'plankton@', password: '12' })
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((err) => console.log(err));
-		// fetchFn({ url: 'auth/register', options: { method: 'POST', body }, onFinish, onError });
-	}, []);
 
 	return { loading, registerWithCredentials };
 };
